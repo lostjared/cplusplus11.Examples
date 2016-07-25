1f5d84e38e6a1031769d95bdc2192d2dbc32200c
lostjared <jaredbruni@gmail.com>
2016-07-25 09:25:04 -0700

    small change to try/catch block

--- tokenize/tokenize.cpp
+++ tokenize/tokenize.cpp
@@ -20,28 +20,27 @@ void outputToken(std::ostream &out, const lex::Token &token) {
 int main() {
     bool active = true;
     while(active == true) {
-        
-        std::string line;
-        std::cout << "Input Text: ";
-        std::getline(std::cin, line);
-        std::istringstream input(line);
-        lex::Scanner scan(input);
-        unsigned int index = 0;
-        while(scan.valid()) {
-            try {
+        try {
+            std::string line;
+            std::cout << "Input Text: ";
+            std::getline(std::cin, line);
+            std::istringstream input(line);
+            lex::Scanner scan(input);
+            unsigned int index = 0;
+            while(scan.valid()) {
                 lex::Token token;
                 scan >> token;
                 ++index;
                 std::cout << "Token Index: #" << index << ": ";
                 outputToken(std::cout, token);
             }
-            catch(lex::Scanner_EOF) {
-                std::cerr << "Scanner End of File.\n";
-                
-            }
-            catch(lex::Scanner_Error) {
-                std::cerr << "Scanner Error..\n";
-            }
+        }
+        catch(lex::Scanner_EOF) {
+            std::cerr << "Scanner End of File.\n";
+            
+        }
+        catch(lex::Scanner_Error) {
+            std::cerr << "Scanner Error..\n";
         }
     }
     return 0;
