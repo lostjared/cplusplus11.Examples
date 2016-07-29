6bc1672060218cc138180dbed5b37c729bac83bb
lostjared <jaredbruni@gmail.com>
2016-07-29 05:10:50 -0700

    updated formatting

--- cgi/tokenize-c++/sourcefile.cpp
+++ cgi/tokenize-c++/sourcefile.cpp
@@ -12,10 +12,10 @@
 std::string urldecode(std::string text);
 
 //! convert text back to HTML
-/*! 
+/*!
  \param text text to convert
  \return tranlsated text
-*/
+ */
 std::string toHTML(const std::string &text) {
     std::string temp;
     for(unsigned int i = 0; i < text.length(); ++i) {
@@ -46,10 +46,10 @@ std::string toHTML(const std::string &text) {
     return temp;
 }
 //! output token
-/*! 
+/*!
  \param out stream to output the html to
  \param t the Token to output
-*/
+ */
 void outputToken(std::ostringstream &out, lex::Token &t) {
     
     static unsigned int counter = 0;
@@ -85,10 +85,10 @@ void outputToken(std::ostringstream &out, lex::Token &t) {
 /*!
  \param argc argument count for main function
  \param argv argument values for main function
-*/
+ */
 int main(int argc, char **argv) {
     bool standard_output = false;
-char *data = 0;
+    char *data = 0;
     std::fstream ufile;
     
     if(argc == 2) {
@@ -101,10 +101,10 @@ char *data = 0;
         std::cout << "Content-type: text/html\n\n";
         data = getenv("QUERY_STRING");
         if((data == NULL) || strlen(data) == 0) {
-        return 0;
+            return 0;
         }
     }
-
+    
     std::ostringstream ofile;
     lex::Scanner *scan;
     if(argc == 2)
@@ -116,24 +116,24 @@ char *data = 0;
         scan = new lex::Scanner(input);
     }
     
-	ofile << "<!DOCTYPE html>\n";
-        ofile << "<html><head><title>" << "C++ Tokenizer " << "</title></head>\n";
-	ofile << "<body>\n";
-	ofile << "<style>\n";
-	ofile << ".linenumber { background-color: rgb(0,100,200); }\n";
-	ofile << ".lineindex_color { color: rgb(255,255,255); background-color: #000000; }\n";
-	ofile << ".linetype { color: rgb(100,0,255); background-color: rgb(25,255,100); }\n";
-	ofile << ".linenum { color: rgb(50,255,50); background-color: #888888; }\n";
-	ofile << ".codetext { color: #000000; background-color: rgb(255,254,208);}\n";
-	ofile << ".codekeyword { color: rgb(0,0,255); font-weight: bold; }\n";
-	ofile << ".codestring { color: rgb(255,0,255);background-color: rgb(200, 200,200); }\n";
- 	ofile << ".codesymbol { color: rgb(76,255,198); background-color: rgb(100,100,100); }\n";
+    ofile << "<!DOCTYPE html>\n";
+    ofile << "<html><head><title>" << "C++ Tokenizer " << "</title></head>\n";
+    ofile << "<body>\n";
+    ofile << "<style>\n";
+    ofile << ".linenumber { background-color: rgb(0,100,200); }\n";
+    ofile << ".lineindex_color { color: rgb(255,255,255); background-color: #000000; }\n";
+    ofile << ".linetype { color: rgb(100,0,255); background-color: rgb(25,255,100); }\n";
+    ofile << ".linenum { color: rgb(50,255,50); background-color: #888888; }\n";
+    ofile << ".codetext { color: #000000; background-color: rgb(255,254,208);}\n";
+    ofile << ".codekeyword { color: rgb(0,0,255); font-weight: bold; }\n";
+    ofile << ".codestring { color: rgb(255,0,255);background-color: rgb(200, 200,200); }\n";
+    ofile << ".codesymbol { color: rgb(76,255,198); background-color: rgb(100,100,100); }\n";
     ofile << ".codedigit { color: rgb(0,255,186); background-color: rgb(0,0,0); }\n";
     ofile << ".codechar { color: rgb(187,212,249); background-color: rgb(94,0,167); }\n";
-	ofile << "</style>\n";
-	ofile << "<a href=\"http://lostsidedead.com\"><img src=\"http://lostsidedead.com/img/logo.png\"></a><br><br>\n\n";
-        ofile << "<table border=\"1\" cellspacing=\"4\" cellpadding=\"4\"><tr style=\"background-color: rgb(150, 150, 150);\"><td class=\"linenumber\"><b>Index</b></td><td class=\"linenumber\"><b>Token</b></td><td class=\"linenumber\"><b>Type</b></td><td class=\"linenumber\"><b>Line Number</b></td></tr>\n";
-
+    ofile << "</style>\n";
+    ofile << "<a href=\"http://lostsidedead.com\"><img src=\"http://lostsidedead.com/img/logo.png\"></a><br><br>\n\n";
+    ofile << "<table border=\"1\" cellspacing=\"4\" cellpadding=\"4\"><tr style=\"background-color: rgb(150, 150, 150);\"><td class=\"linenumber\"><b>Index</b></td><td class=\"linenumber\"><b>Token</b></td><td class=\"linenumber\"><b>Type</b></td><td class=\"linenumber\"><b>Line Number</b></td></tr>\n";
+    
     try {
         while(scan->valid()) {
             lex::Token token;
