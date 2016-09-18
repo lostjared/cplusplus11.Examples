9942976ee28f618a072e1549920e7bf73ce5fa93
lostjared <jaredbruni@gmail.com>
2016-09-18 09:36:56 -0700

    changed example a little

--- UNIX/setjmp/setjmptest.cpp
+++ UNIX/setjmp/setjmptest.cpp
@@ -3,32 +3,29 @@
 
 jmp_buf j_val;
 
-int testF1() {
-    int x = 0;
-    longjmp(j_val, 1); // #1 jump from here back to setjmp return value 1
-    std::cin >> x;
+int testF1(int x) {
+    if(x == 1) longjmp(j_val, 1); // #1 jump from here back to setjmp return value 1
+    ++x;
     return x;
 }
 
-int rtVal() {
-    int x_val = testF1();
+int rtVal(int val) {
+    int x_val = testF1(val);
     ++x_val;
     return x_val;
 }
 
 
-int main() {
-    std::cout << "Enter test: ";
+int main(int argc, char **argv) {
+    std::cout << "Pass no arguments to test longjmp zero to complete function\n\nEnter test: ";
     switch(setjmp(j_val)) { // first call retuns 0 after that value passed to longjmp
         case 0: {
             std::cout << "Returned after inital call.\n";
-            
-            if(rtVal() == 2) {
+            if(rtVal(argc) == 2) {
                 std::cout << "value is 2\n";
             } else {
                 std::cout << "value is not 2\n";
             }
-            
         }
             break;
         case 1: {
