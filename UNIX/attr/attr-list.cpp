02fa70b39ffa26387c9f338692c84e1b5cc537ca
lostjared <jaredbruni@gmail.com>
2016-09-23 15:44:44 -0700

    added macros

--- UNIX/attr/attr-list.cpp
+++ UNIX/attr/attr-list.cpp
@@ -14,8 +14,12 @@ int main(int argc, char **argv) {
     char attr_list[LIST_SIZE], attr_value[LIST_SIZE];
     ssize_t list_len = 0, value_len = 0;
     int cur_pos, i, z;
-    
+#if defined(__APPLE__)
     list_len = listxattr(argv[1], attr_list, LIST_SIZE, XATTR_NOFOLLOW);
+#elif defined(__linux__)
+    list_len = listxattr(argv[1], attr_list, LIST_SIZE);
+#endif
+    
     if(list_len == 1) {
         std::cerr << "Error extend attribute list failed.\n";
         exit(EXIT_FAILURE);
@@ -25,7 +29,12 @@ int main(int argc, char **argv) {
     
     for(cur_pos = 0; cur_pos < list_len; cur_pos += strlen(&attr_list[cur_pos])+1) {
         std::cout << "name = " << &attr_list[cur_pos] << "\n";
+#if defined(__APPLE__)
         value_len = getxattr(argv[1], &attr_list[cur_pos], attr_value,LIST_SIZE, 0, XATTR_NOFOLLOW);
+#elif defined__linux__)
+        value_len = getxattr(argv[1], &attr_list[cur_pos], attr_value,LIST_SIZE);
+#endif
+        
         if(value_len == -1) {
             std::cout << "Could not get value..\n";
         } else {
