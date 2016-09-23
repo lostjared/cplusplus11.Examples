720007a7ccf8c2596eec7b26f70654bc8f941cf1
lostjared <jaredbruni@gmail.com>
2016-09-23 10:19:45 -0700

    added io redirection of stdout/stderr

---
+++ UNIX/io/io-redirect.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cstdio>
+
+int main(int argc, char **argv) {
+    
+    
+    if(argc != 3) {
+        std::cerr << "Error program requires two arguments.\n" << argv[0] << " stdout stderr\n";
+        return 0;
+    }
+    
+    freopen(argv[1], "w", stdout);
+    freopen(argv[2], "w", stderr);
+    system("cat ./io-redirect.cpp");
+    fclose(stdout);
+    fclose(stderr);
+    
+    
+    return 0;
+}
