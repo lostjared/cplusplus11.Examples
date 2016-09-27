9570c8f40719c6d065d136cd503e76348c1d4d90
lostjared <jaredbruni@gmail.com>
2016-09-27 10:40:49 -0700

    added a few timer examples

---
+++ UNIX/timer/alarm.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<signal.h>
+#include<unistd.h>
+#include<sys/time.h>
+#include<ctime>
+#include<errno.h>
+
+void alarmHandler(int sig) {
+    std::cout << "Got alarm..\n";
+    exit(EXIT_SUCCESS);
+}
+
+int main(int argc, char **argv) {
+    
+    struct sigaction sa;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sa.sa_handler = alarmHandler;
+    
+    if(sigaction(SIGALRM, &sa, NULL) == -1) {
+        std::cerr << "Error on signal: " << strerror(errno) << "\n";
+        exit(EXIT_FAILURE);
+    }
+    
+    alarm(4);
+    
+    while(1) {
+        std::cout << "waiting...\n";
+    }
+    
+    return 0;
+    
+}
---
+++ UNIX/timer/timer.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<signal.h>
+#include<sys/time.h>
+#include<ctime>
+#include<unistd.h>
+#include<errno.h>
+
+static volatile sig_atomic_t alarmSet = 0;
+
+void timerHandler(int sig) {
+    alarmSet = 1;
+}
+
+int main(int argc, char **argv) {
+    
+    struct itimerval time_val;
+    clock_t previous;
+    int max_timeouts = 0;
+    int timeouts = 0;
+    
+    struct sigaction sa;
+    
+    std::cout << "Program will now wait for the timer to send a signal..\n";
+    
+    timeouts = 0;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sa.sa_handler = timerHandler;
+    if(sigaction(SIGALRM, &sa, NULL) == -1) {
+        std::cerr << "Signal Error: " << strerror(errno) << "\n";
+        exit(EXIT_FAILURE);
+    }
+    
+    time_val.it_value.tv_sec = 3;
+    time_val.it_value.tv_usec = 1;
+    time_val.it_interval.tv_sec = 3;
+    time_val.it_interval.tv_usec = 1;
+    
+    max_timeouts = 4;
+    if(setitimer(ITIMER_REAL, &time_val, NULL) == -1) {
+        std::cerr << "Set Timer Error:" << strerror(errno) << "\n";
+        exit(EXIT_FAILURE);
+    }
+    
+    previous = clock();
+    timeouts = 0;
+    
+    while(1) {
+        
+        if(alarmSet == 1) {
+            alarmSet = 0;
+            std::cout << "Alarm went off...\n";
+            timeouts++;
+            if(timeouts >= max_timeouts) {
+                std::cout << "Timers captured..\n";
+                exit(EXIT_FAILURE);
+            }
+        }
+        
+        previous = clock();
+    }
+    
+    return 0;
+}
