adff4c5cc0c43d3d51a88d03c12f7d8ff720cf90
lostjared <jaredbruni@gmail.com>
2016-09-28 09:20:06 -0700

    updated example

--- UNIX/timer/nanosleep.cpp
+++ UNIX/timer/nanosleep.cpp
@@ -1,5 +1,24 @@
+/* 
+ 
+ To use this example
+ compile and run it with
+ ./nano-test seconds nanoseconds
+ try something like:
+ ./nano-test 30 200
+ then press Ctrl+C to see how much time is left after the signal.
+
+ */
+
+
+
 #include<iostream>
 #include<ctime>
+#include<signal.h>
+
+
+void handler(int sig) {
+    
+}
 
 int main(int argc, char **argv) {
     
@@ -11,13 +30,24 @@ int main(int argc, char **argv) {
     int seconds = atoi(argv[1]);
     long nanosec = atol(argv[2]);
     
+    struct sigaction sa;
+    
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sa.sa_handler = handler;
+    
+    if(sigaction(SIGINT, &sa, NULL) == -1) {
+        std::cerr << "Error on setting up signal handler..\n";
+        exit(EXIT_FAILURE);
+    }
+    
     struct timespec s, remain;
     s.tv_sec = seconds;
     s.tv_nsec = nanosec;
     std::cout << "Sleeping for: " << seconds << " seconds and " << nanosec << " nanoseconds.\n";
     
     if(nanosleep(&s, &remain) == -1) {
-        std::cout << "Sleep interupted..\n" << remain.tv_sec << " seconds and " << remain.tv_nsec << " nano seconds remaining..\n";
+        std::cout << " Sleep interupted..\n" << remain.tv_sec << " seconds and " << remain.tv_nsec << " nano seconds remaining..\n";
     } else {
         std::cout << "Slept for: " << s.tv_sec << " seconds and " << s.tv_nsec << " nanoseconds\n";
     }
