848b607c5a1ad8514d185300efae5f81076b54c7
lostjared <jaredbruni@gmail.com>
2016-10-02 11:54:29 -0700

    added mutex example

---
+++ UNIX/pthread/pthread-mutex.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<unistd.h>
+#include<pthread.h>
+
+pthread_mutex_t mut = PTHREAD_MUTEX_INITIALIZER;
+
+const int MAX_THREADS = 10;
+
+void *thread_function(void *arg) {
+    long id = (long)arg;
+    for(unsigned int i = 0; i < 100; ++i) {
+        pthread_mutex_lock(&mut);
+        std::cout << "in thread index: " << id << "\n";
+        pthread_mutex_unlock(&mut);
+    }
+    return 0;
+}
+
+
+int main() {
+    std::cout << "Starting threads..\n";
+    pthread_t t_id[MAX_THREADS];
+    
+    for(long i = 0; i < MAX_THREADS; ++i) {
+        pthread_create(&t_id[i], 0, thread_function, (void*)i);
+    }
+    
+    for(int i = 0; i < MAX_THREADS; ++i) {
+        pthread_join(t_id[i], 0);
+    }
+    
+    return EXIT_SUCCESS;
+}
