50f9aefea75f59efb489318414b460eb3c358616
lostjared <jaredbruni@gmail.com>
2016-10-07 19:04:42 -0700

    updated gitopt

---
+++ UNIX/getopt/getopt.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<unistd.h>
+
+
+int main(int argc, char **argv) {
+    
+    if(argc > 1) {
+        int opt;
+        std::cout << "Argument options...\n";
+        
+        while((opt = getopt(argc, argv, "cxi:")) != -1) {
+            switch(opt) {
+                case 'c':
+                    std::cout << "c option\n";
+                    break;
+                case 'x':
+                    std::cout << "x option\n";
+                    break;
+                case 'i':
+                    std::cout << "i option: " << optarg << "\n";
+                    break;
+                    
+            }
+        }
+    }
+    
+    return 0;
+}
