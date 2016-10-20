b534e7552f9c199f56e27f71e20dd04045950701
lostjared <jaredbruni@gmail.com>
2016-10-20 13:19:18 -0700

    updated files

--- UNIX/ipc/posix-shm-write.cpp
+++ UNIX/ipc/posix-shm-write.cpp
@@ -1,8 +1,23 @@
+#include<sys/stat.h>
+#include<fcntl.H>
+#include<sys/mman.h>
+#include<unistd.h>
+#include<cstring>
+#include<errno.h>
+#include<string>
 #include<iostream>
+
+void error(const std::string &text);
+
 
 int main(int argc, char **argv) {
     
     
     
     return 0;
+}
+
+void error(const std::string &text) {
+    std::cerr << "Error: " << text << "\n" << strerror(errno) << "\n";
+    exit(EXIT_FAILURE);
 }
