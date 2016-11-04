b225ba52d77e9f128df3715c02875af257023501
lostjared <jaredbruni@gmail.com>
2016-11-04 13:27:56 -0700

    changed join function to use sstream

--- join/join.hpp
+++ join/join.hpp
@@ -2,17 +2,18 @@
 #define __JOIN_H__
 #include<string>
 #include<iterator>
+#include<sstream>
 
 template<typename T>
 std::string join(const T &type, std::string sep=",") {
-    std::string str_value;
+    std::ostringstream stream;
     for(auto i = std::begin(type); i != std::end(type); ++i) {
         if(i == std::end(type)-1)
-            str_value += *i;
+            stream << *i;
         else
-            str_value += *i + sep;
+            stream << *i << sep;
     }
-    return str_value;
+    return stream.str();
 }
 
 #endif
