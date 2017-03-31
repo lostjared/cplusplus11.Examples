20eb39902fe3ed7546713448ec905c8ce643d820
lostjared <jaredbruni@gmail.com>
2017-03-31 12:56:50 -0700

    added regex example to test string is a ip4 address

---
+++ regex/regex.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<regex>
+#include<string>
+#
+
+int main(int argc, char **argv) {
+    
+    std::cout << "Enter IP Address: ";
+    std::string ip;
+    std::getline(std::cin, ip);
+    
+    if(ip.length() == 0) {
+        std::cerr << "Error nothing entered\n";
+        exit(EXIT_FAILURE);
+    }
+    
+    std::regex r("(\\d{1,3}(\\.\\d{1,3}){3})");
+    bool is_valid = std::regex_match(ip, r);
+    if(is_valid)
+        std::cout << ip << " is a valid IP address\n";
+    else
+        std::cout << ip << " is not a valid IP address\n";
+    
+    return 0;
+}
