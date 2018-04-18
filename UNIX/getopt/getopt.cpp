fd8843250a81b6217e3101771f3054b9f9d663bf
lostjared <jaredbruni@gmail.com>
2018-04-18 09:10:37 -0700

    added small getopt example

--- UNIX/getopt/getopt.cpp
+++ UNIX/getopt/getopt.cpp
@@ -1,13 +1,10 @@
 #include<iostream>
 #include<unistd.h>
 
-
 int main(int argc, char **argv) {
-    
     if(argc > 1) {
         int opt;
         std::cout << "Argument options...\n";
-        
         while((opt = getopt(argc, argv, "cxi:")) != -1) {
             switch(opt) {
                 case 'c':
@@ -19,10 +16,8 @@ int main(int argc, char **argv) {
                 case 'i':
                     std::cout << "i option: " << optarg << "\n";
                     break;
-                    
             }
         }
     }
-    
     return 0;
 }
---
+++ UNIX/getopt/getopt2.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<unistd.h>
+
+class Program {
+public:
+    Program(unsigned int c) : count(c) {}
+    void run();
+private:
+    unsigned int count;
+};
+
+void Program::run() {
+    for(unsigned int i = 0; i < count; ++i) {
+        std::cout << "Hello World with getopt!.\n";
+    }
+}
+
+int main(int argc, char **argv) {
+    if(argc > 1) {
+        int opt;
+        unsigned int counter = 0;
+        while((opt = getopt(argc, argv, "c:")) != -1) {
+            switch(opt) {
+                case 'c':
+                    counter = atoi(optarg);
+                    if(counter == 0) {
+                        std::cerr << "Error requires loop count...\n";
+                        exit(EXIT_FAILURE);
+                    }
+                    break;
+                default:
+                    std::cout << " use -c counter\n";
+                    break;
+            }
+        }
+        Program program(counter);
+        program.run();
+    } else {
+        std::cerr << "use: " << argv[0] << " -c counter\n";
+    }
+    return 0;
+}
